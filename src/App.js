b58240c98ad017d1e6e36768a05b0012b4241e08
Hồng Ngọc Doãn <hongngocdoan@Nhims-MBP.home>
2019-06-05 17:56:28 +0300

    Implemented keeping user logged in after closing app

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ import {
   Redirect,
   NavLink
 } from 'react-router-dom'
+import jwtDecode from 'jwt-decode'
 
 import store from './store'
 import Nav from './containers/Nav'
 import Auth from './containers/Auth'
+import setAuthToken from './actions/setAuthToken'
+import { setUser } from './actions/authActions'
 
 import './App.scss'
+
+if (localStorage.token) {
+  const token = localStorage.token
+  setAuthToken(token)
+  const user = jwtDecode(token)
+  store.dispatch(setUser(user))
+}
 
 class App extends React.Component {
   render() {
