b0a31e59e4a92d3f71bf0cbf0212d7fcac9310a1
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-06-23 22:51:48 +0300

    Rewrote App component using Hooks

--- src/App.js
+++ src/App.js
@@ -1,83 +1,83 @@
-import React from 'react'
-import { Provider } from 'react-redux'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import jwtDecode from 'jwt-decode'
 
-import store from './store'
-import Header from './containers/Header'
-import Nav from './containers/Nav'
-import Auth from './containers/Auth'
-import MainHome from './containers/MainHome'
-import MainDecks from './containers/MainDecks'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Auth from './components/Auth'
+import MainHome from './components/MainHome'
+import MainDecks from './components/MainDecks'
 import setAuthToken from './actions/setAuthToken'
-import { setUser, logoutUser } from './actions/authActions'
-import { getAllDecks } from './actions/deckActions'
+import { AuthContext } from './contexts/AuthContext'
+import isEmpty from './utils/isEmpty'
 
 import './App.scss'
 
-const token = localStorage.token
+export default function App(props) {
+  const { auth, authDispatch, setUser, logoutUser } = React.useContext(
+    AuthContext
+  )
 
-if (token) {
-  setAuthToken(token)
-  const user = jwtDecode(token)
-  store.dispatch(setUser(user))
-  store.dispatch(getAllDecks())
+  const token = localStorage.token
+  let user = {}
 
-  const currentTime = Date.now() / 1000
-  if (user.exp < currentTime) {
-    store.dispatch(logoutUser())
-  }
-}
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      store.getState().auth.isAuthenticated ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />
-      )
+  if (!isEmpty(token)) {
+    setAuthToken(token)
+    user = jwtDecode(token)
+    const currentTime = Date.now() / 1000
+    if (user.exp < currentTime) {
+      logoutUser()
     }
-  />
-)
-
-class App extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <Route exact path='/auth' basename='/auth' component={Auth} />
-
-          <div className='App container'>
-            <PrivateRoute
-              exact
-              path='/(|decks|categories|setting)'
-              component={Header}
-            />
-
-            <div className='aside'>
-              <PrivateRoute
-                exact
-                path='/(|decks|categories|setting)'
-                component={Nav}
-              />
-            </div>
-
-            <div className='main'>
-              <PrivateRoute exact path='/' basename='/' component={MainHome} />
-              <PrivateRoute
-                exact
-                path='/decks'
-                basename='/decks'
-                component={MainDecks}
-              />
-            </div>
-          </div>
-        </Router>
-      </Provider>
-    )
   }
-}
 
-export default App
+  useEffect(() => {
+    authDispatch(setUser(user))
+  }, [])
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        auth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/auth', state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  )
+
+  return (
+    <Router>
+      <Route exact path='/auth' basename='/auth' component={Auth} />
+
+      <div className='App container'>
+        <PrivateRoute
+          exact
+          path='/(|decks|categories|setting)'
+          component={Header}
+        />
+
+        <div className='aside'>
+          <PrivateRoute
+            exact
+            path='/(|decks|categories|setting)'
+            component={Nav}
+          />
+        </div>
+
+        <div className='main'>
+          <PrivateRoute exact path='/' basename='/' component={MainHome} />
+          <PrivateRoute
+            exact
+            path='/decks'
+            basename='/decks'
+            component={MainDecks}
+          />
+        </div>
+      </div>
+    </Router>
+  )
+}
