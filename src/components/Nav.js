aa075ea5bfb1a258cbc59b19312f8d4c279b5070
ngocdh236 <ngocdh236@gmail.com>
2019-06-25 13:31:45 +0300

    Fixed Nav UI

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,50 +8,47 @@ import iconSetting from '../assets/iconSetting.svg'
 
 export default function Nav() {
   return (
-    <div className='d-flex'>
-      <table className='Nav'>
-        <tbody>
-          <tr>
-            <th />
-            <td>
-              <img src={ava} className='mb-5' alt='Avatar' />
-            </td>
-          </tr>
+    <table className='Nav'>
+      <tbody>
+        <tr>
+          <th />
+          <td>
+            <img src={ava} className='mb-5' alt='Avatar' />
+          </td>
+        </tr>
 
-          <tr>
-            <th>
-              <img src={iconDecks} alt='Decks' />
-            </th>
-            <td>
-              <NavLink
-                className='nav-link nav-item-bold'
-                activeClassName='a-active'
-                exact
-                to='/decks'
-              >
-                All Decks
-              </NavLink>
-            </td>
-          </tr>
+        <tr>
+          <th>
+            <img src={iconDecks} alt='Decks' />
+          </th>
+          <td>
+            <NavLink
+              className='nav-link nav-item-bold'
+              activeClassName='a-active'
+              exact
+              to='/decks'
+            >
+              All Decks
+            </NavLink>
+          </td>
+        </tr>
 
-          <tr>
-            <th>
-              <img src={iconSetting} alt='Setting' />
-            </th>
-            <td>
-              <NavLink
-                className='nav-link nav-item-bold'
-                activeClassName='a-active'
-                exact
-                to='/setting'
-              >
-                Setting
-              </NavLink>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div className='vertical-line mx-5' />
-    </div>
+        <tr>
+          <th>
+            <img src={iconSetting} alt='Setting' />
+          </th>
+          <td>
+            <NavLink
+              className='nav-link nav-item-bold'
+              activeClassName='a-active'
+              exact
+              to='/setting'
+            >
+              Setting
+            </NavLink>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
