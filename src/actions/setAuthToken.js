5cbf52835ab529674deea5ae6a1292b9c56e1140
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-07-27 09:39:20 +0300

    Refactored code

--- src/actions/setAuthToken.js
+++
@@ -1,9 +0,0 @@
-import customAxios from './customAxios'
-
-export default token => {
-  if (token) {
-    customAxios.defaults.headers.common['Authorization'] = token
-  } else {
-    delete customAxios.defaults.headers.common['Authorization']
-  }
-}
--- src/services/customAxios.js
+++ src/services/customAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const customAxios = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://sheltered-reaches-53167.herokuapp.com/api'
 })
 
 export default customAxios
