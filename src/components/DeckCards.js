ccd180700c2fe7bd4e27114bf822cd90cd43cdfd
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-10-25 13:41:29 +0300

    Updated UI

--- src/components/DeckCards.js
+++ src/components/DeckCards.js
@@ -9,7 +9,7 @@ export default function DeckCards({ deck }) {
   const allCards = cards.map(card => <CardButton key={card.id} card={card} />);
 
   return (
-    <div className="DeckCards main">
+    <div className="DeckCards">
       <div className="cards">
         {allCards}
         <CardButton newCard={true} />
