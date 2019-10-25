3e063159e9f6b6f55f54dc3344c2faf8e037bfee
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-10-25 13:40:30 +0300

    Fixed UI, updated deck service

--- src/services/deckService.js
+++ src/services/deckService.js
@@ -1,41 +1,54 @@
-import customAxios from './customAxios'
-import { Types } from '../reducers/actionTypes'
+import customAxios from './customAxios';
+import { Types } from '../reducers/actionTypes';
 
 export const useService = (state, dispatch) => {
-  const deckUrl = '/decks'
+  const deckUrl = '/decks';
 
   const create = deck => {
     customAxios
       .post(deckUrl, deck)
       .then(res => dispatch({ type: Types.CREATE_DECK, deck: res.data }))
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
-  const getAll = () => {
-    customAxios
-      .get(deckUrl)
-      .then(res => dispatch({ type: Types.SET_DECKS, decks: res.data }))
-      .catch(err => console.log(err))
-  }
+  const getAll = field => {
+    var type;
+    switch (field) {
+      case '-updatedAt':
+        type = Types.SET_RECENT_DECKS;
+        break;
+      default:
+        type = Types.SET_DECKS;
+        break;
+    }
 
-  const update = (deckId, deck) => {
     customAxios
-      .put(`${deckUrl}/${deckId}`, deck)
+      .get(`${deckUrl}?field=${field}`)
+      .then(res => dispatch({ type, decks: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  const getById = id => customAxios.get(`${deckUrl}/${id}`);
+
+  const update = deck => {
+    customAxios
+      .put(deckUrl, deck)
       .then(res => dispatch({ type: Types.UPDATE_DECK, deck: res.data }))
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
   const remove = deckId => {
     customAxios
       .delete(`${deckUrl}/${deckId}`)
       .then(res => dispatch({ type: Types.DELETE_DECK, deckId }))
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
   return {
     create,
     getAll,
+    getById,
     update,
     remove
-  }
-}
+  };
+};
