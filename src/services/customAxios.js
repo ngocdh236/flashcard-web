d40b8a58d80e8505e5743dfbcb53a5809df06c36
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-10-29 15:28:19 +0200

    Change axios baseURL

--- src/services/customAxios.js
+++ src/services/customAxios.js
@@ -1,7 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const customAxios = axios.create({
-  baseURL: 'http://localhost:5000/api'
-})
+  // baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://sheltered-reaches-53167.herokuapp.com/api'
+});
 
-export default customAxios
+export default customAxios;
